dd9ae69e198ec0763dd9e5f616985736a3a38e83
Octavia Efraim <octavia.efraim2023@campus-eni.fr>
2023-08-09 11:23:02 +0200

    Create project

---
+++ src/main/java/fr/eni/potager/PotagerApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.potager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PotagerApplication.class, args);
+  }
+
+}
