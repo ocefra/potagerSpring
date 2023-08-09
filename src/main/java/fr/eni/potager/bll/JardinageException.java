49e258048ed2be62174884b8a7177d49714d9e73
Teck Tea <teckos@hotmail.fr>
2023-08-10 00:36:04 +0200

    Tweak businessException + runnable

--- src/main/java/fr/eni/potager/bll/JardinageException.java
+++ src/main/java/fr/eni/potager/bll/JardinageException.java
@@ -1,7 +1,19 @@
 package fr.eni.potager.bll;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JardinageException extends Exception {
+  List<String> listError = new ArrayList<>();
+
   public JardinageException(String message) {
     super(message);
+  }
+  public JardinageException(List<String> listError) {
+    this.listError = listError;
+  }
+
+  public List<String> getListError() {
+    return listError;
   }
 }
